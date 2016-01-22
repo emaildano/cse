0abbc56972784662005b03e046a4f9016f09a67f
Daniel Olson <emaildano@gmail.com>
2016-01-22 17:34:14 -0500

    Update results.php

--- results.php
+++ results.php
@@ -1,36 +1,15 @@
 <?php
-$query = rawurlencode($_GET['q']);
-$args = 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' . $query;
-$site_url = 'http://myphillypark.org';
+$query    = rawurlencode($_GET['s']);
+$key      = '123abc';
+$cx       = '123abc';
+$url      = 'https://www.googleapis.com/customsearch/v1?key='.$key.'&cx='.$cx.'&q='.$query;
+$content  = file_get_contents($url);
+$json     = json_decode($content, true);
 
-// sendRequest
-// note how referer is set manually
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $args);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_REFERER, $site_url);
-$body = curl_exec($ch);
-curl_close($ch);
+// echo '<pre>';
+//   print_r($json);
+// echo '</pre>';
 
-// Json Decode
-$json = json_decode($body);
-
-// Json Output
-// print_r($json);
-
-// Query
-echo 'Search Query: ' . $query . '<hr>';
-
-foreach( $json->responseData->results as $result) {
-  echo '<div style="padding-bottom: 20px;">';
-  echo $result->url . '<br>';
-  echo $result->title . '<br>';
-  echo $result->content . '<br>';
-  echo $result->url . '<br>';
-  echo $result->cacheUrl . '<br>';
-  echo '<div><hr>';
+foreach($json['items'] as $item) {
+  ...
 }
-
-
-
-?>
